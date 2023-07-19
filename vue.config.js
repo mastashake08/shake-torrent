85eec1b3eeb032fc54dc5ce10965f2ed9ebccd70
Hacker God <jyrone.parker@gmail.com>
2023-07-19 04:46:03 -0400

    webtorrent can download and view torrents

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+/**
+ * Created by kelvinsun on 18/9/29.
+ *
+ * vue-cli config file.
+ */
+
+'use strict';
+
+
+module.exports = {
+    // 因为这个项目是打包发布的 app, 不是线上资源, 所以不需要哈希
+    outputDir: 'dist'
+};
